a68a0575b5f55bdfd01e01b9918241ef2ec126cf
reinol martin <reinolmartin01@gmail.com>
2025-08-22 18:29:29 +0300

    new changes: modified artwork-manager.tsx

--- components/artwork-manager.tsx
+++ components/artwork-manager.tsx
@@ -232,7 +232,7 @@ export function ArtworkManager({ uploadLimit, currentCount }: ArtworkManagerProp
       return
     }
 
-    if (files.length > ) {
+    if (files.length > 500) {
       alert("You can upload a maximum of 15 files at once.")
       return
     }
